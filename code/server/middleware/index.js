d94637add23d368ca00de0c64f72e4fa0e0d32fd
YYY <2601178727@qq.com>
2020-06-22 09:57:41 +0800

    fix:跨域

--- code/server/middleware/index.js
+++ code/server/middleware/index.js
@@ -10,8 +10,8 @@ import Func from './func'
 export default app => {
     //跨域
     app.use(async (ctx, next) => { 
-        ctx.set('Access-Control-Allow-Origin', 'http://yangyiyuan.online');
-        ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+        ctx.set('Access-Control-Allow-Origin', '*');
+        ctx.set('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Authorization , Access-Control-Request-Headers');
         ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
         if (ctx.method == 'OPTIONS') {
             ctx.body = 200; 
