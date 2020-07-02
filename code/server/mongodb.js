8e2ad43cdca434f4dd4d4db5e61fd9ca0bf9bd33
YYY <2601178727@qq.com>
2020-07-02 14:52:54 +0800

    save

--- code/server/mongodb.js
+++ code/server/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import conf from './config'
 // const DB_URL = `mongodb://${conf.mongodb.address}/${conf.mongodb.db}`
-const DB_URL = `mongodb://${conf.mongodb.username}:${conf.mongodb.pwd}@${conf.mongodb.address}/${conf.mongodb.db}`; // 账号登陆
+const DB_URL = `mongodb://${conf.mongodb.username}:${conf.mongodb.pwd}@${conf.mongodb.address}/${conf.mongodb.db}?authSource=admin`; // 账号登陆
 mongoose.Promise = global.Promise
 mongoose.connect(DB_URL, { useMongoClient: true }, err => {
     if (err) {
